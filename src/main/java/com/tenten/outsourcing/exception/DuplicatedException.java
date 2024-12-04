e91143c6daec53b19c1979d4b1a7af9c938af702
K-da-fire <105974458+K-da-fire@users.noreply.github.com>
2024-12-04 18:20:26 +0900

    feature : "로그인 기능 구현"

---
+++ src/main/java/com/tenten/outsourcing/exception/DuplicatedException.java
@@ -0,0 +1,11 @@
+package com.tenten.outsourcing.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DuplicatedException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+}
