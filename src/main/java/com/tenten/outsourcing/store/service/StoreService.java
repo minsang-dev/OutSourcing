203f22d137123b9d6e9d6039ec02e77f6b1d4a31
Hyeonjoon Kim <hyeon00n@naver.com>
2024-12-05 14:43:42 +0900

    feat: store 수정 기능 추가 및 store 예외처리 적용

--- src/main/java/com/tenten/outsourcing/store/service/StoreService.java
+++ src/main/java/com/tenten/outsourcing/store/service/StoreService.java
@@ -1,8 +1,13 @@
 package com.tenten.outsourcing.store.service;
 
 import com.tenten.outsourcing.common.Auth;
+import com.tenten.outsourcing.exception.InvalidInputException;
+import com.tenten.outsourcing.exception.NoAuthorizedException;
+import com.tenten.outsourcing.exception.NotFoundException;
 import com.tenten.outsourcing.store.dto.StoreRequestDto;
 import com.tenten.outsourcing.store.dto.StoreResponseDto;
+import com.tenten.outsourcing.store.dto.StoreUpdateRequestDto;
+import com.tenten.outsourcing.store.dto.StoreUpdateResponseDto;
 import com.tenten.outsourcing.store.entity.Store;
 import com.tenten.outsourcing.store.repository.StoreRepository;
 import com.tenten.outsourcing.user.entity.User;
@@ -14,8 +19,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static com.tenten.outsourcing.exception.ErrorCode.*;
+
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StoreService {
     private final StoreRepository storeRepository;
     private final UserRepository userRepository;
@@ -23,13 +31,15 @@ public class StoreService {
     @Transactional
     public StoreResponseDto create(Long userId, StoreRequestDto requestDto) {
 
-        User findUser = userRepository.findById(userId).orElseThrow();
+        User findUser = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(NO_SESSION));
         List<Store> stores = storeRepository.findByUserId(userId);
 
         if (!Auth.OWNER.equals(findUser.getAuth())) {
+            throw new NoAuthorizedException(NO_AUTHOR_USER);
         }
 
         if (!(stores.size() < 3)) {
+            throw new InvalidInputException(STORE_REGISTRATION_LIMITED);
         }
 
         Store store = storeRepository.save(new Store(requestDto, findUser));
@@ -43,5 +53,29 @@ public class StoreService {
         storeResponseDtoPage = storeRepository.findByNameLike(name, pageable).stream().map(StoreResponseDto::new).toList();
 
         return storeResponseDtoPage;
+    }
+
+    @Transactional
+    public StoreUpdateResponseDto updateById(Long userId, Long storeId, StoreUpdateRequestDto requestDto) {
+        User findUser = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(NO_SESSION));
+
+        if (!Auth.OWNER.equals(findUser.getAuth())) {
+            throw new NoAuthorizedException(NO_AUTHOR_USER);
+        }
+
+        List<Store> ownStores = storeRepository.findByUserId(userId);
+
+        Store findStore = storeRepository.findById(storeId).orElseThrow(() -> new NotFoundException(NOT_FOUND_STORE));
+
+        List<Store> matchedStore = ownStores.stream().filter(store -> store.getId().equals(findStore.getId())).toList();
+
+        if (!matchedStore.isEmpty()) {
+            matchedStore.get(0).updateStoreInformation(requestDto);
+
+        } else {
+            throw new NoAuthorizedException(NOT_FOUND_STORE);
+        }
+
+        return new StoreUpdateResponseDto(matchedStore.get(0));
     }
 }
