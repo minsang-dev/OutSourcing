38d046708fd170ece06d9794ee9416798495f62e
KM4HS <ac4hsu@gmail.com>
2024-12-06 11:52:11 +0900

    feature: 장바구니 삭제

--- src/main/java/com/tenten/outsourcing/bucket/controller/BucketController.java
+++ src/main/java/com/tenten/outsourcing/bucket/controller/BucketController.java
@@ -27,6 +27,14 @@ public class BucketController {
 
     private final BucketService bucketService;
 
+    /**
+     * 장바구니 생성
+     *
+     * @param response     쿠키 생성을 위한 response
+     * @param dto          메뉴 id, 가게 id
+     * @param loginSession 현재 로그인한 유저 세션
+     * @param cookie       장바구니 쿠키가 있는 경우 받아옴
+     */
     @PostMapping
     public ResponseEntity<String> createBucket(
             HttpServletResponse response,
@@ -48,6 +56,12 @@ public class BucketController {
         return new ResponseEntity<>("장바구니에 메뉴가 추가되었습니다.", HttpStatus.CREATED);
     }
 
+    /**
+     * 자신의 장바구니 조회
+     *
+     * @param loginSession 현재 로그인한 유저 세션
+     * @param cookie       장바구니 쿠키가 있으면 받아옴
+     */
     @GetMapping
     public ResponseEntity<List<BucketResponseDto>> findBuckets(
             @SessionAttribute(name = LoginStatus.LOGIN_USER) SessionDto loginSession,
@@ -61,4 +75,27 @@ public class BucketController {
         return ResponseEntity.ok().body(bucketsDto);
     }
 
+    /**
+     * 장바구니 메뉴 삭제. (개수 변경 X)
+     *
+     * @param loginSession 현재 로그인한 유저 세션
+     * @param cookie       쿠키 장바구니 목록
+     * @param dto          메뉴 Id, 가게  Id
+     */
+    @DeleteMapping
+    public ResponseEntity<String> deleteBucket(
+            HttpServletResponse response,
+            @SessionAttribute(name = LoginStatus.LOGIN_USER) SessionDto loginSession,
+            @CookieValue(name = "bucket") Cookie cookie,
+            @RequestBody @Valid BucketRequestDto dto
+    ) {
+
+        String jsonString = bucketService.deleteBucket(loginSession.getId(), URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8), dto.getMenuId());
+
+        Cookie newCookie = new Cookie("bucket", URLEncoder.encode(jsonString, StandardCharsets.UTF_8));
+        newCookie.setMaxAge(60 * 60 * 24); // 하루
+        response.addCookie(newCookie);
+
+        return ResponseEntity.ok().body("장바구니에서 메뉴가 삭제되었습니다.");
+    }
 }
--- src/main/java/com/tenten/outsourcing/bucket/service/BucketService.java
+++ src/main/java/com/tenten/outsourcing/bucket/service/BucketService.java
@@ -5,6 +5,7 @@ import com.tenten.outsourcing.bucket.dto.BucketResponseDto;
 import com.tenten.outsourcing.bucket.entity.Bucket;
 import com.tenten.outsourcing.menu.entity.Menu;
 import com.tenten.outsourcing.menu.service.MenuService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -42,9 +43,7 @@ public class BucketService {
             List<Bucket> bucketList = new ArrayList<>();
             boolean isDuplicated = false;
             if (!cookieValue.isEmpty()) {
-                bucketList = new ArrayList<>(Bucket.jsonStringToBuckets(cookieValue).stream()
-                        .filter(b -> b.getUserId().equals(userId))
-                        .toList());
+                bucketList = filterBucketsByUser(Bucket.jsonStringToBuckets(cookieValue), userId);
 
                 // 장바구니에 추가하려는 메뉴가 같은 가게의 메뉴가 맞을 경우
                 if (bucketList.get(0).getStoreId().equals(findMenu.getStore().getId())) {
@@ -79,14 +78,54 @@ public class BucketService {
         }
 
         try {
-            List<Bucket> usersList = new ArrayList<>(Bucket.jsonStringToBuckets(cookieValue).stream()
-                    .filter(b -> b.getUserId().equals(userId))
-                    .toList());
+            List<Bucket> usersList = filterBucketsByUser(Bucket.jsonStringToBuckets(cookieValue), userId);
 
             return usersList.stream().map(BucketResponseDto::new).toList();
         } catch (JsonProcessingException e) {
             log.info(e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    /**
+     * 장바구니 삭제. Count 무관하게 메뉴 통으로 삭제
+     *
+     * @param userId 유저 식별자
+     * @param cookieValue 쿠키 값
+     * @param menuId 삭제할 메뉴 식별자
+     */
+    public String deleteBucket(Long userId, String cookieValue, Long menuId) {
+        try {
+            List<Bucket> usersList = filterBucketsByUser(Bucket.jsonStringToBuckets(cookieValue), userId);
+
+            boolean isExist = false;
+            for(Bucket b : usersList) {
+                if(b.getMenuId().equals(menuId)) {
+                    usersList.remove(b);
+                    isExist = true;
+                    break;
+                }
+            }
+
+            if(!isExist) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
+
+            return Bucket.BucketToJsonString(usersList);
+        } catch (JsonProcessingException e) {
+            log.info(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 특정 유저의 장바구니만 필터링
+     *
+     * @param list   쿠키에 있던 장바구니들
+     * @param userId 유저 식별자
+     * @return {@code userId}에 해당하는 장바구니 목록
+     */
+    public List<Bucket> filterBucketsByUser(List<Bucket> list, Long userId) {
+        return list.stream().filter(b -> b.getUserId().equals(userId)).toList();
     }
 }
